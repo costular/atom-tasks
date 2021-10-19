0d36602ce418f4723c271e9800de0a7458279417
Diego Francisco <costular@gmail.com>
2021-10-19 18:21:04 +0100

    Fix material dialog API

--- app/src/main/java/com/costular/atomhabits/ui/features/habits/create/CreateHabitReminder.kt
+++ app/src/main/java/com/costular/atomhabits/ui/features/habits/create/CreateHabitReminder.kt
@@ -3,23 +3,20 @@ package com.costular.atomhabits.ui.features.habits.create
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.selection.selectable
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.runtime.*
-import com.costular.atomhabits.domain.model.Reminder
 import com.vanpra.composematerialdialogs.MaterialDialog
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.dp
 import com.costular.atomhabits.R
 import com.costular.atomhabits.ui.components.ScreenHeader
 import com.costular.atomhabits.ui.util.DateTimeFormatters
-import com.vanpra.composematerialdialogs.buttons
-import com.vanpra.composematerialdialogs.datetime.timepicker.timepicker
+import com.vanpra.composematerialdialogs.datetime.time.timepicker
+import com.vanpra.composematerialdialogs.rememberMaterialDialogState
 import java.time.LocalTime
 
 @Composable
@@ -28,17 +25,17 @@ fun CreateHabitReminder(
     onChangeReminderEnabled: (Boolean) -> Unit,
     onChangeReminderTime: (LocalTime) -> Unit
 ) {
-    val dialog = remember { MaterialDialog() }
-
-    dialog.build {
-        timepicker(initialTime = state.reminderTime) { time ->
-            onChangeReminderTime(time)
-        }
-        buttons {
+    val dialogState = rememberMaterialDialogState()
+    MaterialDialog(
+        dialogState = dialogState,
+        buttons = {
             positiveButton("Ok")
             negativeButton("Cancel")
         }
-
+    ) {
+        timepicker(initialTime = state.reminderTime) { time ->
+            onChangeReminderTime(time)
+        }
     }
 
     Box(modifier = Modifier.fillMaxSize()) {
@@ -90,7 +87,7 @@ fun CreateHabitReminder(
                         modifier = Modifier
                             .matchParentSize()
                             .clickable(
-                                onClick = { dialog.show() },
+                                onClick = { dialogState.show() },
                                 interactionSource = interactionSource,
                                 indication = null
                             )
