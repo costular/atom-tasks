62154d457f42bd5f8028c0d15300a6a0d68618d2
Diego Francisco <costular@gmail.com>
2021-10-19 18:14:14 +0100

    Fix plugin versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ buildscript {
         maven("https://plugins.gradle.org/m2/")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.1.0-alpha02")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
+        classpath("com.android.tools.build:gradle:7.0.3")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
         classpath("com.google.dagger:hilt-android-gradle-plugin:${Versions.hilt}")
     }
 }
