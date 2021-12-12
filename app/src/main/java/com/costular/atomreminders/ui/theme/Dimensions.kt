d67eb9930794eb1608bfd06a4dafa8e69966ef88
Diego Francisco <costular@gmail.com>
2021-12-12 00:29:07 +0000

    Add more dimens

--- app/src/main/java/com/costular/atomreminders/ui/theme/Dimensions.kt
+++ app/src/main/java/com/costular/atomreminders/ui/theme/Dimensions.kt
@@ -4,17 +4,37 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 class Dimensions(
-    val contentMargin: Dp
+    val contentMargin: Dp,
+    val spacingSmall: Dp,
+    val spacingMedium: Dp,
+    val spacingLarge: Dp,
+    val spacingXLarge: Dp,
+    val spacingHuge: Dp
 )
 
 val CompactDimensions = Dimensions(
-    contentMargin = 16.dp
+    contentMargin = 16.dp,
+    spacingSmall = 4.dp,
+    spacingMedium = 8.dp,
+    spacingLarge = 16.dp,
+    spacingXLarge = 24.dp,
+    spacingHuge = 32.dp
 )
 
 val MediumDimensions = Dimensions(
-    contentMargin = 32.dp
+    contentMargin = 32.dp,
+    spacingSmall = 4.dp,
+    spacingMedium = 8.dp,
+    spacingLarge = 16.dp,
+    spacingXLarge = 24.dp,
+    spacingHuge = 32.dp,
 )
 
 val ExpandedDimensions = Dimensions(
-    contentMargin = 200.dp
+    contentMargin = 200.dp,
+    spacingSmall = 4.dp,
+    spacingMedium = 8.dp,
+    spacingLarge = 16.dp,
+    spacingXLarge = 24.dp,
+    spacingHuge = 32.dp,
 )
