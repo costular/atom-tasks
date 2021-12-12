561dbffa17ddccf10f3a5be35cdb7d3ea2ec06a7
Diego Francisco <costular@gmail.com>
2021-12-12 00:32:25 +0000

    Refactor: view model

--- app/src/main/java/com/costular/atomreminders/ui/features/tasks/detail/TaskDetailViewModel.kt
+++ app/src/main/java/com/costular/atomreminders/ui/features/tasks/detail/TaskDetailViewModel.kt
@@ -8,14 +8,13 @@ import com.costular.atomreminders.domain.InvokeStarted
 import com.costular.atomreminders.domain.InvokeSuccess
 import com.costular.atomreminders.domain.interactor.GetTaskByIdInteractor
 import com.costular.atomreminders.domain.interactor.RemoveTaskInteractor
-import com.costular.decorit.presentation.base.MviViewModel
+import com.costular.atomreminders.ui.mvi.MviViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import timber.log.Timber
 import javax.inject.Inject
 
