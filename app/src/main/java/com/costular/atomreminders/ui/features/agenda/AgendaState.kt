5633d574cf3bedbaf7e8e829ea5bff36b373a499
Diego Francisco <costular@gmail.com>
2021-12-21 19:09:36 +0000

    Add tests

--- app/src/main/java/com/costular/atomreminders/ui/features/agenda/AgendaState.kt
+++ app/src/main/java/com/costular/atomreminders/ui/features/agenda/AgendaState.kt
@@ -2,9 +2,18 @@ package com.costular.atomreminders.ui.features.agenda
 
 import com.costular.atomreminders.domain.Async
 import com.costular.atomreminders.domain.model.Task
+import com.costular.atomreminders.ui.features.agenda.AgendaViewModel.Companion.DaysAfter
+import com.costular.atomreminders.ui.features.agenda.AgendaViewModel.Companion.DaysBefore
 import java.time.LocalDate
 
 data class AgendaState(
     val selectedDay: LocalDate = LocalDate.now(),
-    val tasks: Async<List<Task>> = Async.Uninitialized
-)
+    val tasks: Async<List<Task>> = Async.Uninitialized,
+) {
+    val calendarFromDate = LocalDate.now().minusDays(DaysBefore.toLong())
+    val calendarUntilDate = LocalDate.now().plusDays(DaysAfter.toLong())
+
+    val isPreviousDaySelected get() = calendarFromDate.isBefore(selectedDay)
+    val isNextDaySelected get() = calendarUntilDate.isAfter(selectedDay)
+
+}
