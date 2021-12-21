f86f0e9622d16287d211e44f6ef184f71fe5a0c5
Diego Francisco <costular@gmail.com>
2021-12-21 20:46:08 +0000

    Fix db tests

--- app/src/androidTest/java/com/costular/atomreminders/data/db/TaskDatabaseTest.kt
+++ app/src/androidTest/java/com/costular/atomreminders/data/db/TaskDatabaseTest.kt
@@ -4,12 +4,11 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import app.cash.turbine.test
-import com.costular.atomreminders.data.habitrecord.HabitRecordDao
-import com.costular.atomreminders.data.habitrecord.HabitRecordEntity
 import com.costular.atomreminders.data.tasks.TaskEntity
 import com.costular.atomreminders.data.tasks.TasksDao
 import com.costular.atomreminders.db.AtomRemindersDatabase
 import com.google.common.truth.Truth
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.asExecutor
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.runBlockingTest
@@ -21,6 +20,7 @@ import java.io.IOException
 import java.time.LocalDate
 import kotlin.time.ExperimentalTime
 
+@ExperimentalCoroutinesApi
 @ExperimentalTime
 @RunWith(AndroidJUnit4::class)
 class TaskDatabaseTest {
@@ -29,7 +29,6 @@ class TaskDatabaseTest {
 
     private lateinit var db: AtomRemindersDatabase
     private lateinit var tasksDao: TasksDao
-    private lateinit var habitRecordDao: HabitRecordDao
 
     @Before
     fun setUp() {
@@ -42,7 +41,6 @@ class TaskDatabaseTest {
             .allowMainThreadQueries()
             .build()
         tasksDao = db.getHabitsDao()
-        habitRecordDao = db.getHabitRecordDao()
     }
 
     @After
@@ -58,55 +56,17 @@ class TaskDatabaseTest {
             0L,
             LocalDate.now(),
             "whatever",
-            TaskEntity.REPETITION_TYPE_DAILY,
-            null
-        )
-
-        // When
-        val id = tasksDao.addHabit(habit)
-
-        // Then
-        tasksDao.getAllTasks().test {
-            Truth.assertThat(expectItem().size).isEqualTo(1)
-            cancelAndIgnoreRemainingEvents()
-        }
-    }
-
-    @Test
-    fun testAddHabitRecord() = testCoroutine.runBlockingTest {
-        // Given
-        val habit = TaskEntity(
-            0L,
             LocalDate.now(),
-            "whatever",
-            TaskEntity.REPETITION_TYPE_DAILY,
-            null
+            true
         )
 
         // When
-        val id = tasksDao.addHabit(habit)
-
-        val records = listOf(
-            HabitRecordEntity(
-                0L,
-                LocalDate.now(),
-                id
-            ),
-            HabitRecordEntity(
-                0L,
-                LocalDate.now().plusDays(1),
-                id
-            )
-        )
-        records.forEach { record ->
-            habitRecordDao.addEntry(record)
-        }
+        val id = tasksDao.addTask(habit)
 
         // Then
         tasksDao.getAllTasks().test {
-            val habit = expectItem()
-            Truth.assertThat(habit.size).isEqualTo(1)
-            Truth.assertThat(habit.first().records.size).isEqualTo(records.size)
+            Truth.assertThat(awaitItem().size).isEqualTo(1)
+            cancelAndIgnoreRemainingEvents()
         }
     }
 
