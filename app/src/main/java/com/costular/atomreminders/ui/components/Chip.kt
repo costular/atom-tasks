e592318b21cf51e09d7b37a496a71bf677fda0f6
Diego Francisco <costular@gmail.com>
2021-12-24 17:40:30 +0000

    Update chip component

--- app/src/main/java/com/costular/atomreminders/ui/components/Chip.kt
+++ app/src/main/java/com/costular/atomreminders/ui/components/Chip.kt
@@ -4,11 +4,14 @@ import android.graphics.Paint
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CalendarToday
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -17,31 +20,35 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.costular.atomreminders.R
+import com.costular.atomreminders.ui.theme.AppTheme
 import com.costular.atomreminders.ui.theme.AtomRemindersTheme
 
 @Composable
 fun Chip(
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
+    isSelected: Boolean = false,
+    border: BorderStroke = BorderStroke(1.dp, MaterialTheme.colors.onSurface),
     backgroundColor: Color = MaterialTheme.colors.background,
-    isError: Boolean = false,
+    contentPadding: PaddingValues = PaddingValues(start = 8.dp, end = 8.dp),
     content: @Composable RowScope.() -> Unit,
 ) {
-    val borderColor =
-        if (isError) MaterialTheme.colors.error else contentColorFor(backgroundColor).copy(alpha = ContentAlpha.disabled)
+    val shape = MaterialTheme.shapes.small.copy(all = CornerSize(8.dp))
+    val computedBackgroundColor = if (isSelected) MaterialTheme.colors.primary else backgroundColor
 
     Surface(
-        color = backgroundColor,
-        border = BorderStroke(
-            1.dp,
-            borderColor,
-        ),
-        shape = MaterialTheme.shapes.small,
+        color = computedBackgroundColor,
+        shape = shape,
+        border = border,
         modifier = modifier
-            .clip(MaterialTheme.shapes.small)
+            .height(32.dp)
+            .clip(shape)
             .clickable(onClick = onClick),
     ) {
-        Row(Modifier.padding(16.dp), verticalAlignment = Alignment.CenterVertically) {
+        Row(
+            Modifier.padding(contentPadding),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
             CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
                 content()
             }
@@ -56,9 +63,10 @@ private fun ChipPreview() {
         Chip(onClick = {}) {
             Icon(
                 imageVector = Icons.Default.CalendarToday,
-                contentDescription = null
+                contentDescription = null,
+                modifier = Modifier.size(AppTheme.ChipIconSize)
             )
-            Spacer(Modifier.width(16.dp))
+            Spacer(Modifier.width(8.dp))
             Text(stringResource(R.string.today), style = MaterialTheme.typography.body1)
         }
     }
