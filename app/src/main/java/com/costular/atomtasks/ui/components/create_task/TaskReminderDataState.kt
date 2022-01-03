c6192abb84901c50bfbe5c033c806ad5a406ff20
Diego Francisco <costular@gmail.com>
2022-01-03 00:47:05 +0000

    Show reminder enabled by default

--- app/src/main/java/com/costular/atomtasks/ui/components/create_task/TaskReminderDataState.kt
+++ app/src/main/java/com/costular/atomtasks/ui/components/create_task/TaskReminderDataState.kt
@@ -1,7 +1,7 @@
 package com.costular.atomtasks.ui.components.create_task
 
 data class TaskReminderDataState(
-    val isEnabled: Boolean = false,
+    val isEnabled: Boolean = true,
 ) {
     companion object {
         val Empty = TaskReminderDataState()
