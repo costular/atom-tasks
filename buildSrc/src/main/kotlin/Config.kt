c010c6d9c34e3fe9997d9c7f53a1d61ed7518abd
Diego Francisco <costular@gmail.com>
2022-01-08 18:53:46 +0000

    Bump version

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -2,6 +2,6 @@ object Config {
     val compileVersion = 31
     val minSdk = 21
     val targetSdk = 30
-    val versionCode = 1
-    val versionName = "0.1.0"
+    val versionCode = 2
+    val versionName = "0.2.0"
 }
