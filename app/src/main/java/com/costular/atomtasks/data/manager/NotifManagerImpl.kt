da53cfb6483baf87b391af0fd87af55d90497919
Diego Francisco <costular@gmail.com>
2022-01-08 00:09:55 +0000

    Add: open app when tap on notification

--- app/src/main/java/com/costular/atomtasks/data/manager/NotifManagerImpl.kt
+++ app/src/main/java/com/costular/atomtasks/data/manager/NotifManagerImpl.kt
@@ -1,14 +1,21 @@
 package com.costular.atomtasks.data.manager
 
 import android.app.Notification
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import com.costular.atomtasks.MainActivity
 import com.costular.atomtasks.R
 import com.costular.atomtasks.domain.manager.NotifManager
 import com.costular.atomtasks.domain.model.Task
 import com.costular.atomtasks.ui.util.CHANNEL_REMINDERS
 
+@ExperimentalComposeUiApi
+@ExperimentalMaterialApi
 class NotifManagerImpl(private val context: Context) : NotifManager {
 
     private val notificationManager: NotificationManagerCompat =
@@ -23,6 +30,15 @@ class NotifManagerImpl(private val context: Context) : NotifManager {
                 NotificationCompat.BigTextStyle()
                     .bigText(task.name)
             )
+            .setContentIntent(
+                PendingIntent.getActivity(
+                    context,
+                    REQUEST_OPEN_APP,
+                    Intent(context, MainActivity::class.java),
+                    PendingIntent.FLAG_UPDATE_CURRENT,
+                    null,
+                )
+            )
 
         notify(task.id.toInt(), builder.build())
     }
@@ -34,5 +50,9 @@ class NotifManagerImpl(private val context: Context) : NotifManager {
     private fun buildNotificationBase(channel: String): NotificationCompat.Builder =
         NotificationCompat.Builder(context, channel)
             .setSmallIcon(R.mipmap.ic_launcher)
+
+    companion object {
+        const val REQUEST_OPEN_APP = 20
+    }
 
 }
