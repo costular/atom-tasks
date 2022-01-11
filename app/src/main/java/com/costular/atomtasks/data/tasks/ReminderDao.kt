f923413a7b5810681fd6bea344ccb668e3f37afb
Diego Francisco <costular@gmail.com>
2022-01-11 23:31:10 +0000

    Add: repository method to update task's reminder

--- app/src/main/java/com/costular/atomtasks/data/tasks/ReminderDao.kt
+++ app/src/main/java/com/costular/atomtasks/data/tasks/ReminderDao.kt
@@ -3,11 +3,19 @@ package com.costular.atomtasks.data.tasks
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import java.time.LocalTime
 
 @Dao
 abstract class ReminderDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     abstract suspend fun insertReminder(reminderEntity: ReminderEntity)
+
+    @Query("UPDATE reminders SET time = :time WHERE task_id = :taskId")
+    abstract suspend fun updateReminder(taskId: Long, time: LocalTime)
+
+    @Query("DELETE FROM reminders WHERE task_id = :taskId")
+    abstract suspend fun removeReminder(taskId: Long)
 
 }
