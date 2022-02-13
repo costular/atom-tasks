82f317348a1c92cf7b35b467ef66e36a8d5712be
Diego Francisco <costular@gmail.com>
2022-02-13 14:12:02 +0000

    test: use mocked data and add more tests for different scenarios

--- app/src/androidTest/java/com/costular/atomtasks/ui/features/agenda/AgendaTest.kt
+++ app/src/androidTest/java/com/costular/atomtasks/ui/features/agenda/AgendaTest.kt
@@ -1,28 +1,23 @@
 package com.costular.atomtasks.ui.features.agenda
 
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.ui.ExperimentalComposeUiApi
 import com.costular.atomtasks.R
+import com.costular.atomtasks.domain.model.Task
+import com.costular.atomtasks.domain.repository.TasksRepository
 import com.costular.atomtasks.ui.base.AndroidTest
 import com.costular.atomtasks.ui.base.getString
-import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
-import org.junit.Before
-import org.junit.Rule
+import io.mockk.coEvery
+import io.mockk.coVerify
+import kotlinx.coroutines.flow.flowOf
 import org.junit.Test
+import java.time.LocalDate
+import javax.inject.Inject
 
-@OptIn(ExperimentalMaterialApi::class)
-@ExperimentalComposeUiApi
 @HiltAndroidTest
 class AgendaTest : AndroidTest() {
 
-    @get:Rule(order = 0)
-    val hiltRule = HiltAndroidRule(this)
-
-    @Before
-    fun setUp() {
-        hiltRule.inject()
-    }
+    @Inject
+    lateinit var taskRepository: TasksRepository
 
     @Test
     fun shouldChangeSelectedDay_whenClickPrevDay() {
@@ -51,7 +46,7 @@ class AgendaTest : AndroidTest() {
 
     @Test
     fun shouldCreateATask_whenClickOnCreateNewTaskTypeAndThenSave() {
-        val taskName = "this is a test :D"
+        val taskName = "whatever"
 
         agenda {
             openCreateTask {
@@ -62,7 +57,90 @@ class AgendaTest : AndroidTest() {
         }
 
         agenda {
-            firstTaskHasText(taskName)
+        }
+    }
+
+    @Test
+    fun shouldShowTaskInList_whenLandOnScreen() {
+        val taskName = "this is a test :D"
+        val tasks = listOf(
+            Task(
+                1L,
+                taskName,
+                LocalDate.now(),
+                null,
+                true,
+            )
+        )
+        coEvery { taskRepository.getTasks(LocalDate.now()) } returns flowOf(tasks)
+
+        agenda {
+            taskHasText(0, taskName)
+        }
+    }
+
+    @Test
+    fun shouldShowTaskMarkedAsDone_whenLandOnScreen() {
+        val taskName = "this is a test :D"
+        val isDone = true
+
+        val tasks = listOf(
+            Task(
+                1L,
+                taskName,
+                LocalDate.now(),
+                null,
+                isDone,
+            )
+        )
+        coEvery { taskRepository.getTasks(LocalDate.now()) } returns flowOf(tasks)
+
+        agenda {
+            taskIsDone(taskName, isDone)
+        }
+    }
+
+    @Test
+    fun shouldShowTaskMarkedAsNotDone_whenLandOnScreen() {
+        val taskName = "this is a test :D"
+        val isDone = false
+
+        val tasks = listOf(
+            Task(
+                1L,
+                taskName,
+                LocalDate.now(),
+                null,
+                isDone,
+            )
+        )
+        coEvery { taskRepository.getTasks(LocalDate.now()) } returns flowOf(tasks)
+
+        agenda {
+            taskIsDone(taskName, isDone)
+        }
+    }
+
+    @Test
+    fun shouldToggleTaskStatus_whenClickOnTaskCheckbox() {
+        val id = 1L
+        val taskName = "this is a test :D"
+        val isDone = false
+
+        val tasks = listOf(
+            Task(
+                id,
+                taskName,
+                LocalDate.now(),
+                null,
+                isDone,
+            )
+        )
+        coEvery { taskRepository.getTasks(LocalDate.now()) } returns flowOf(tasks)
+
+        agenda {
+            toggleTask(taskName)
+            coVerify { taskRepository.markTask(id, !isDone) }
         }
     }
 
