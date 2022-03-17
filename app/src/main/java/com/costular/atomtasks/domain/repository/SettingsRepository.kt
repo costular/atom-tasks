28fbeb65d1e4cf137801c88f51c318fb05ad99a1
Diego <diegooo.fc@gmail.com>
2022-03-17 16:49:14 +0000

    feat: add settings repository

--- app/src/main/java/com/costular/atomtasks/domain/repository/SettingsRepository.kt
+++ app/src/main/java/com/costular/atomtasks/domain/repository/SettingsRepository.kt
@@ -1,5 +1,9 @@
 package com.costular.atomtasks.domain.repository
 
-interface SettingsRepository {
+import com.costular.atomtasks.domain.model.Theme
+import kotlinx.coroutines.flow.Flow
 
+interface SettingsRepository {
+    fun observeTheme(): Flow<Theme>
+    suspend fun setTheme(theme: Theme)
 }
