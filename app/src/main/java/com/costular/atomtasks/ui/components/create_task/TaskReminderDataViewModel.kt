8a2bba5a1a2e78f2ab4ef589a789626d4d102b05
Diego <diegooo.fc@gmail.com>
2022-03-18 12:31:46 +0000

    lint: remove empty constructor

--- app/src/main/java/com/costular/atomtasks/ui/components/create_task/TaskReminderDataViewModel.kt
+++ app/src/main/java/com/costular/atomtasks/ui/components/create_task/TaskReminderDataViewModel.kt
@@ -2,7 +2,7 @@ package com.costular.atomtasks.ui.components.create_task
 
 import com.costular.atomtasks.ui.mvi.MviViewModel
 
-class TaskReminderDataViewModel() : MviViewModel<TaskReminderDataState>(TaskReminderDataState.Empty) {
+class TaskReminderDataViewModel : MviViewModel<TaskReminderDataState>(TaskReminderDataState.Empty) {
 
     fun enableReminder(isEnabled: Boolean) {
         setState {
