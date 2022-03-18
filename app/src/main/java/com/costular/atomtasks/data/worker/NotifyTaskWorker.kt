69e25957d2c415b886e145774ae6674482a031ba
Diego <diegooo.fc@gmail.com>
2022-03-18 21:07:42 +0000

    lint: disable too generic exception for this file

--- app/src/main/java/com/costular/atomtasks/data/worker/NotifyTaskWorker.kt
+++ app/src/main/java/com/costular/atomtasks/data/worker/NotifyTaskWorker.kt
@@ -14,6 +14,7 @@ import java.lang.Exception
 import java.lang.IllegalArgumentException
 import java.lang.IllegalStateException
 
+@Suppress("TooGenericExceptionCaught")
 @HiltWorker
 class NotifyTaskWorker @AssistedInject constructor(
     @Assisted appContext: Context,
