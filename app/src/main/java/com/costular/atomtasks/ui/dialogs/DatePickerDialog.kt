47773140e7ad878c85732d0ea48e374d9f8a4349
Diego Francisco Concepción <diego.concepcion@jobandtalent.com>
2022-05-03 00:44:29 +0100

    feature: create new date picker

---
+++ app/src/main/java/com/costular/atomtasks/ui/dialogs/DatePickerDialog.kt
@@ -0,0 +1,50 @@
+package com.costular.atomtasks.ui.dialogs
+
+import android.app.DatePickerDialog
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.window.Dialog
+import com.costular.atomtasks.R
+import com.costular.atomtasks.ui.components.DatePicker
+import com.costular.atomtasks.ui.theme.AppTheme
+import java.time.LocalDate
+
+@Composable
+fun DatePickerDialog(
+    onDismiss: () -> Unit,
+    currentDate: LocalDate,
+    onDateSelected: (LocalDate) -> Unit,
+) {
+    Dialog(onDismissRequest = onDismiss) {
+        Surface(
+            color = MaterialTheme.colors.background,
+            shape = MaterialTheme.shapes.medium,
+        ) {
+            Column(modifier = Modifier.padding(AppTheme.dimens.contentMargin)) {
+                Text(
+                    text = stringResource(R.string.create_task_set_date),
+                    style = MaterialTheme.typography.h5,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
+                    modifier = Modifier
+                        .padding(top = AppTheme.dimens.spacingMedium)
+                        .padding(bottom = AppTheme.dimens.spacingXLarge),
+                )
+
+                DatePicker(
+                    currentDate = currentDate,
+                    onDateSelected = onDateSelected,
+                )
+            }
+        }
+    }
+}
