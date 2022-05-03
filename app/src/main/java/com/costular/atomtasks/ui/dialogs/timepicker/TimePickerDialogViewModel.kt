6ef612c3445a4028d4c1f44f08f9b4b71b6cd708
Diego Francisco Concepción <diego.concepcion@jobandtalent.com>
2022-05-03 18:23:23 +0100

    feat: add time picker view model

---
+++ app/src/main/java/com/costular/atomtasks/ui/dialogs/timepicker/TimePickerDialogViewModel.kt
@@ -0,0 +1,24 @@
+package com.costular.atomtasks.ui.dialogs.timepicker
+
+import com.costular.atomtasks.ui.mvi.MviViewModel
+import java.time.LocalTime
+
+class TimePickerDialogViewModel : MviViewModel<TimePickerDialogState>(TimePickerDialogState.Empty) {
+
+    fun setTime(time: LocalTime) {
+        setState {
+            copy(time = time)
+        }
+    }
+
+    fun cancel() {
+        sendEvent(TimePickerUiEvents.Cancel)
+    }
+
+    fun save() {
+        withState {
+            sendEvent(TimePickerUiEvents.Save(time))
+        }
+    }
+
+}
