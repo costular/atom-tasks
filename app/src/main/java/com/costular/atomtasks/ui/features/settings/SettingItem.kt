5a2a58fc42f354a710d1c55466bb3cc251160c08
Diego Francisco Concepción <diego.concepcion@jobandtalent.com>
2022-05-07 16:17:03 +0100

    refactor: package

--- app/src/main/java/com/costular/atomtasks/ui/features/settings/SettingItem.kt
+++ app/src/main/java/com/costular/atomtasks/ui/features/settings/SettingItem.kt
@@ -1,4 +1,4 @@
-package com.costular.atomtasks.ui.settings
+package com.costular.atomtasks.ui.features.settings
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
--- app/src/main/java/com/costular/atomtasks/ui/features/settings/SettingLink.kt
+++ app/src/main/java/com/costular/atomtasks/ui/features/settings/SettingLink.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.costular.atomtasks.ui.features.settings.SettingItem
 import com.costular.atomtasks.ui.theme.AtomRemindersTheme
 
 @Composable
