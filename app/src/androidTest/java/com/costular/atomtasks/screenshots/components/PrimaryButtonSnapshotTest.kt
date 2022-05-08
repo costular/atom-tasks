4a6f5c5cd5fa6ec41815beb0212f044516046a3d
Diego Francisco Concepción <diego.concepcion@jobandtalent.com>
2022-05-08 15:49:42 +0100

    refactor: move snapshot test to another package

--- app/src/androidTest/java/com/costular/atomtasks/screenshots/components/PrimaryButtonSnapshotTest.kt
+++ app/src/androidTest/java/com/costular/atomtasks/screenshots/components/PrimaryButtonSnapshotTest.kt
@@ -1,7 +1,8 @@
-package com.costular.atomtasks.ui.components
+package com.costular.atomtasks.screenshots.components
 
 import androidx.compose.material.Text
 import com.costular.atomtasks.ui.base.SnapshotTest
+import com.costular.atomtasks.ui.components.PrimaryButton
 import org.junit.Test
 
 class PrimaryButtonSnapshotTest : SnapshotTest() {
