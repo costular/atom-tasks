8b3f3738600b3882e71ff75ffb1502c53be17f22
Diego Francisco Concepción <diego.concepcion@jobandtalent.com>
2022-05-10 16:12:44 +0100

    test: add UI tests for task card

---
+++ app/src/androidTest/java/com/costular/atomtasks/ui/components/TaskCardTest.kt
@@ -0,0 +1,109 @@
+package com.costular.atomtasks.ui.components
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import com.costular.atomtasks.domain.model.Reminder
+import com.costular.atomtasks.ui.base.AndroidTest
+import dagger.hilt.android.testing.HiltAndroidTest
+import io.mockk.mockk
+import io.mockk.verify
+import java.time.LocalDate
+import java.time.LocalTime
+import org.junit.Test
+
+@HiltAndroidTest
+class TaskCardTest : AndroidTest() {
+
+    @Test
+    fun shouldShowText() {
+        val text = "this is a test :)"
+
+        composeTestRule.setContent {
+            TaskCard(
+                title = text,
+                isFinished = false,
+                reminder = null,
+                onMark = { /*TODO*/ },
+                onOpen = { /*TODO*/ },
+            )
+        }
+
+        composeTestRule.onNodeWithText(text)
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun shouldShowReminder_whenReminderIsSet() {
+        val text = "whatever"
+        val reminderTime = LocalTime.of(9, 0)
+        val reminder = Reminder(
+            1L,
+            reminderTime,
+            true,
+            LocalDate.now()
+        )
+        val reminderText = reminderTime.toString()
+
+        composeTestRule.setContent {
+            TaskCard(
+                title = text,
+                isFinished = false,
+                reminder = reminder,
+                onMark = { /*TODO*/ },
+                onOpen = { /*TODO*/ },
+            )
+        }
+
+        composeTestRule.onNodeWithText("[alarm] $reminderText")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun shouldCallMarkCallback_whenTapOnMarkable() {
+        val onMarkCallback: () -> Unit = mockk(relaxed = true)
+
+        composeTestRule.setContent {
+            TaskCard(
+                title = "whatever",
+                isFinished = false,
+                reminder = null,
+                onMark = onMarkCallback,
+                onOpen = { /*TODO*/ },
+            )
+        }
+
+        composeTestRule.onNodeWithTag("Markable")
+            .performClick()
+
+        verify {
+            onMarkCallback()
+        }
+    }
+
+    @Test
+    fun shouldCallClickCallback_whenTapOnAnywhere() {
+        val text = "whatever"
+        val onClickCallback: () -> Unit = mockk(relaxed = true)
+
+        composeTestRule.setContent {
+            TaskCard(
+                title = text,
+                isFinished = true,
+                reminder = null,
+                onMark = { /*TODO*/ },
+                onOpen = onClickCallback,
+            )
+        }
+
+        composeTestRule.onNodeWithText(text)
+            .assertIsDisplayed()
+            .performClick()
+
+        verify {
+            onClickCallback()
+        }
+    }
+
+}
