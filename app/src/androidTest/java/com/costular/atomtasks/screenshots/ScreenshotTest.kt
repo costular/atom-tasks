546c870c4bce8b1b8ec098d4cb964d6db8a3e0a1
Diego Francisco Concepción <diego.concepcion@jobandtalent.com>
2022-05-27 17:46:25 +0100

    feat: Use annotation instead of package to run screenshot tests

---
+++ app/src/androidTest/java/com/costular/atomtasks/screenshots/ScreenshotTest.kt
@@ -0,0 +1,5 @@
+package com.costular.atomtasks.screenshots
+
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
+@Retention
+annotation class ScreenshotTest
--- app/src/androidTest/java/com/costular/atomtasks/screenshots/components/PrimaryButtonSnapshotTest.kt
+++ app/src/androidTest/java/com/costular/atomtasks/screenshots/components/PrimaryButtonSnapshotTest.kt
@@ -1,10 +1,12 @@
 package com.costular.atomtasks.screenshots.components
 
 import androidx.compose.material.Text
+import com.costular.atomtasks.screenshots.ScreenshotTest
 import com.costular.atomtasks.ui.base.SnapshotTest
 import com.costular.atomtasks.ui.components.PrimaryButton
 import org.junit.Test
 
+@ScreenshotTest
 class PrimaryButtonSnapshotTest : SnapshotTest() {
 
     @Test
