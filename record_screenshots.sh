10f493e2c7fffb7b30a49daeb535d0932052d15f
Diego Francisco Concepción <diego.concepcion@jobandtalent.com>
2022-06-01 19:29:21 +0100

    Revert "fix: fail if gradle task does not pass"
    This reverts commit e00864ff1fbe26db66e7cf3163e382c265e3f0e3.

--- record_screenshots.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-adb shell settings put global hidden_api_policy_pre_p_apps 1
-adb shell settings put global hidden_api_policy_p_apps 1
-adb shell settings put global hidden_api_policy 1
-
-bash disable_animations.sh
-
-./gradlew clean executeScreenshotTests -Pandroid.testInstrumentationRunnerArguments.annotation=com.costular.atomtasks.screenshots.ScreenshotTest -Precord
--- verify_screenshots.sh
+++ verify_screenshots.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-mkdir testResults
-adb logcat > testResults/logcat.txt &
-
 adb shell settings put global hidden_api_policy_pre_p_apps 1
 adb shell settings put global hidden_api_policy_p_apps 1
 adb shell settings put global hidden_api_policy 1
@@ -9,7 +6,4 @@ adb shell settings put global hidden_api_policy 1
 bash disable_animations.sh
 
 ./gradlew clean executeScreenshotTests \
--Pandroid.testInstrumentationRunnerArguments.annotation=com.costular.atomtasks.screenshots.ScreenshotTest || touch sorry_but_tests_are_failing
-
-mv app/build/reports/androidTests/connected testResults/
-test ! -f sorry_but_tests_are_failing
+-Pandroid.testInstrumentationRunnerArguments.annotation=com.costular.atomtasks.screenshots.ScreenshotTest
