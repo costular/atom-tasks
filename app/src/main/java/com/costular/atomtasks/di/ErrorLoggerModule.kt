bb5589032e666ae7203d16fdacd251223e33723b
Diego Francisco Concepción <diego.concepcion@jobandtalent.com>
2022-06-19 23:55:52 +0100

    feat: log non-fatal errors in order to track them on Crashlytics

--- app/src/main/java/com/costular/atomtasks/di/ErrorLoggerModule.kt
+++ app/src/main/java/com/costular/atomtasks/di/ErrorLoggerModule.kt
@@ -2,17 +2,17 @@ package com.costular.atomtasks.di
 
 import com.costular.atomtasks.data.manager.ErrorLoggerImpl
 import com.costular.atomtasks.domain.manager.ErrorLogger
-import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
-abstract class ErrorLoggerModule {
+class ErrorLoggerModule {
 
     @Singleton
-    @Binds
-    abstract fun provideErrorLogger(errorLoggerImpl: ErrorLoggerImpl): ErrorLogger
+    @Provides
+    fun provideErrorLogger(): ErrorLogger = ErrorLoggerImpl()
 }
