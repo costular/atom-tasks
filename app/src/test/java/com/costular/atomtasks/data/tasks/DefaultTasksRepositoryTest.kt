ffd4edf4f749eccd614654c237888f3ca13ef687
Diego Francisco Concepción <diego.concepcion@jobandtalent.com>
2022-06-20 00:07:28 +0100

    style: fix lint errors

--- app/src/test/java/com/costular/atomtasks/data/tasks/DefaultTasksRepositoryTest.kt
+++ app/src/test/java/com/costular/atomtasks/data/tasks/DefaultTasksRepositoryTest.kt
@@ -66,7 +66,7 @@ class DefaultTasksRepositoryTest {
                     taskReminderTime,
                     taskDate,
                     reminderEnabled,
-                    taskId
+                    taskId,
                 )
             }
         }
