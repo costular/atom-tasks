8d2ed89dddbaac3d77b040f757a55a3994c320cc
Diego Francisco Concepción <diego.concepcion@jobandtalent.com>
2022-06-21 17:50:25 +0100

    refactor: remove unused code

--- buildSrc/src/main/kotlin/Gradle.kt
+++ buildSrc/src/main/kotlin/Gradle.kt
@@ -1,9 +1,7 @@
 object GradlePlugins {
     val android = "com.android.application"
-    val androidLibrary = "com.android.library"
     val kotlin = "org.jetbrains.kotlin.jvm"
     val kotlinParcelize = "kotlin-parcelize"
     val safeArgs = "androidx.navigation.safeargs.kotlin"
-    val ktint = "org.jlleitschuh.gradle.ktlint"
     val hilt = "dagger.hilt.android.plugin"
 }
