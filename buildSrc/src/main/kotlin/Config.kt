7dae9d33a61468b131be70cef14b89a48a9300f3
Diego Francisco Concepción <diego.concepcion@jobandtalent.com>
2022-06-22 17:45:59 +0100

    bump: app version

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -2,6 +2,6 @@ object Config {
     val compileVersion = 31
     val minSdk = 21
     val targetSdk = 30
-    val versionCode = 7
-    val versionName = "0.6.0"
+    val versionCode = 8
+    val versionName = "0.7.0"
 }
