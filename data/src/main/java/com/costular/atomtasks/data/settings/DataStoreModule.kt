a87f7683a2040e86e37f3184a8be559a2af919b7
Diego Francisco Concepción <diego.concepcion@jobandtalent.com>
2022-07-08 16:58:22 +0100

    refactor: add custom coroutine scope using dispatcher provider

--- data/src/main/java/com/costular/atomtasks/data/settings/DataStoreModule.kt
+++ data/src/main/java/com/costular/atomtasks/data/settings/DataStoreModule.kt
@@ -1,4 +1,4 @@
-package com.costular.atomtasks.di
+package com.costular.atomtasks.data.settings
 
 import android.content.Context
 import androidx.datastore.core.DataStore
@@ -7,12 +7,15 @@ import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.preferencesDataStoreFile
+import com.costular.core.net.DispatcherProvider
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 
 @InstallIn(SingletonComponent::class)
 @Module
@@ -20,12 +23,16 @@ class DataStoreModule {
 
     @Singleton
     @Provides
-    fun provideDataStore(@ApplicationContext appContext: Context): DataStore<Preferences> =
+    fun provideDataStore(
+        @ApplicationContext appContext: Context,
+        dispatcherProvider: DispatcherProvider,
+    ): DataStore<Preferences> =
         PreferenceDataStoreFactory.create(
             corruptionHandler = ReplaceFileCorruptionHandler(
                 produceNewData = { emptyPreferences() },
             ),
             produceFile = { appContext.preferencesDataStoreFile(PREFERENCES_NAME) },
+            scope = CoroutineScope(dispatcherProvider.io + SupervisorJob())
         )
 
     private companion object {
