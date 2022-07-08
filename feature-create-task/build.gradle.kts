29484d32bbb97dc25553051a973a447d2b6f8c37
Diego Francisco Concepción <diego.concepcion@jobandtalent.com>
2022-07-08 17:37:05 +0100

    feat: add "create task" module

---
+++ feature-create-task/build.gradle.kts
@@ -0,0 +1,77 @@
+plugins {
+    id(GradlePlugins.androidLibrary)
+    id(GradlePlugins.kotlinAndroid)
+    kotlin("kapt")
+    id("com.google.devtools.ksp") version "1.6.10-1.0.4"
+}
+
+android {
+    compileSdk = Config.compileVersion
+    defaultConfig {
+        minSdk = Config.minSdk
+    }
+
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = Versions.compose
+    }
+
+    ksp {
+        arg("compose-destinations.mode", "destinations")
+        arg("compose-destinations.moduleName", "createtask")
+    }
+}
+
+kotlin {
+    sourceSets {
+        debug {
+            kotlin.srcDir("build/generated/ksp/debug/kotlin")
+        }
+        release {
+            kotlin.srcDir("build/generated/ksp/release/kotlin")
+        }
+    }
+}
+
+dependencies {
+    implementation(project(":common-ui"))
+    implementation(project(":data"))
+
+    implementation(Deps.composeActivity)
+    implementation(Deps.composeFoundation)
+    implementation(Deps.composeRuntime)
+    implementation(Deps.composeLayout)
+    implementation(Deps.composeMaterial)
+    implementation(Deps.composeMaterialIcons)
+    implementation(Deps.composeUi)
+    implementation(Deps.composeUiTooling)
+    implementation(Deps.accompanistSystemUi)
+    implementation(Deps.accompanistInsetsUi)
+    implementation(Deps.viewModel)
+    implementation(Deps.hiltNavigationCompose)
+    implementation(Deps.hiltJetpackViewModel)
+    kapt(Deps.hiltJetpackCompiler)
+    implementation(Deps.composeDestinations)
+    ksp(Deps.composeDestinationsKsp)
+
+    implementation(Deps.numberPicker)
+    implementation(Deps.lottie)
+    implementation(Deps.composeCalendar)
+    implementation(Deps.accompanistPager)
+    implementation(Deps.accompanistPagerIndicators)
+
+    testImplementation(project(":core-testing"))
+}
