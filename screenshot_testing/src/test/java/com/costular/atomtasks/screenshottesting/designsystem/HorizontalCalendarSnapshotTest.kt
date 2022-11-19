bd1ec4daaa3800bcc4db4787d5dadbda87418a6e
Diego Francisco Concepción <diego.concepcion@jobandtalent.com>
2022-11-19 20:31:40 +0000

    Create and record horizontal calendar screenshot tests

---
+++ screenshot_testing/src/test/java/com/costular/atomtasks/screenshottesting/designsystem/HorizontalCalendarSnapshotTest.kt
@@ -0,0 +1,40 @@
+package com.costular.atomtasks.screenshottesting.designsystem
+
+import app.cash.paparazzi.Paparazzi
+import com.costular.atomtasks.screenshottesting.utils.FontSize
+import com.costular.atomtasks.screenshottesting.utils.PaparazziFactory
+import com.costular.atomtasks.screenshottesting.utils.Theme
+import com.costular.atomtasks.screenshottesting.utils.asFloat
+import com.costular.atomtasks.screenshottesting.utils.isDarkTheme
+import com.costular.atomtasks.screenshottesting.utils.screenshot
+import com.costular.designsystem.components.HorizontalCalendar
+import com.google.testing.junit.testparameterinjector.TestParameter
+import com.google.testing.junit.testparameterinjector.TestParameterInjector
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(TestParameterInjector::class)
+class HorizontalCalendarSnapshotTest {
+
+    @TestParameter
+    private lateinit var fontScale: FontSize
+
+    @TestParameter
+    private lateinit var themeMode: Theme
+
+    @get:Rule
+    val paparazzi: Paparazzi = PaparazziFactory.create()
+
+    @Test
+    fun horizontalCalendarTest() {
+        paparazzi.screenshot(
+            isDarkTheme = themeMode.isDarkTheme(),
+            fontScale = fontScale.asFloat(),
+        ) {
+            HorizontalCalendar(
+                onSelectDay = {},
+            )
+        }
+    }
+}
