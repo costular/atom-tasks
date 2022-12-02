229b5405e5fbec35034f73949963811c4c1572e1
Diego Francisco Concepción <diego.concepcion@jobandtalent.com>
2022-12-02 17:45:12 +0000

    Fix Text import to use Material 3

--- common/tasks/src/main/java/com/costular/atomtasks/tasks/TaskList.kt
+++ common/tasks/src/main/java/com/costular/atomtasks/tasks/TaskList.kt
@@ -11,8 +11,8 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.Text
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
