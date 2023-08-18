159136ed64e21911af73a90e789c9a34b803f672
Diego Francisco Concepción <diego.concepcion@jobandtalent.com>
2023-08-18 23:14:37 +0100

    Improve multi preview

--- core/ui/src/main/java/com/costular/atomtasks/coreui/utils/VariantsPreview.kt
+++ core/ui/src/main/java/com/costular/atomtasks/coreui/utils/VariantsPreview.kt
@@ -1,17 +1,8 @@
 package com.costular.atomtasks.core.ui.utils
 
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
-import androidx.compose.ui.tooling.preview.Preview
+import com.costular.atomtasks.coreui.utils.DarkPreview
+import com.costular.atomtasks.coreui.utils.LargeFontPreview
 
-@Preview(
-    name = "large font",
-    group = "font scales",
-    fontScale = 1.5f,
-)
-@Preview(
-    name = "dark theme",
-    group = "themes",
-    uiMode = UI_MODE_NIGHT_YES,
-    showBackground = true,
-)
+@LargeFontPreview
+@DarkPreview
 annotation class VariantsPreview
