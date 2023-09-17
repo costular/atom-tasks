73b494b3a9aa8ac6475a699770da0391d6d544d6
Diego Francisco Concepción <diego.concepcion@jobandtalent.com>
2023-09-17 20:58:55 +0100

    Move time formatter out of the composable

--- core/src/main/java/com/costular/core/util/DateTimeFormatters.kt
+++ core/src/main/java/com/costular/core/util/DateTimeFormatters.kt
@@ -1,16 +1,16 @@
 package com.costular.core.util
 
+import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
 import java.util.Locale
 
 object DateTimeFormatters {
-    val dayOfWeekFormatter: DateTimeFormatter =
-        DateTimeFormatter.ofPattern("EEE", Locale.getDefault())
     val shortDayOfWeekFormatter =
         DateTimeFormatter.ofPattern("EEEEE", Locale.getDefault())
-    val fullDayOfWeekFormatter =
-        DateTimeFormatter.ofPattern("EEEE", Locale.getDefault())
     val dateFormatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM)
     val monthFormatter = DateTimeFormatter.ofPattern("MMMM yyyy")
+
+    fun LocalTime.formatTime(locale: Locale): String =
+        DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT).withLocale(locale).format(this)
 }
