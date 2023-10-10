ac2736f86165b995308e7b6badb6087c1d8513e7
Diego Francisco Concepción <diego.concepcion@jobandtalent.com>
2023-10-10 23:03:59 +0100

    Avoid launching many instances of the MainActivity when opening the app

--- app/src/main/java/com/costular/atomtasks/data/TaskNotificationManagerImpl.kt
+++ app/src/main/java/com/costular/atomtasks/data/TaskNotificationManagerImpl.kt
@@ -62,7 +62,9 @@ class TaskNotificationManagerImpl(
                 PendingIntent.getActivity(
                     context,
                     REQUEST_OPEN_APP,
-                    Intent(context, MainActivity::class.java),
+                    Intent(context, MainActivity::class.java).apply {
+                        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    },
                     pendingIntentFlag,
                     null,
                 ),
