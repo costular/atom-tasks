7d6fd374239ffea8b4e22da10e35f473b63c3a8b
Diego Francisco Concepción <diego.concepcion@jobandtalent.com>
2023-10-23 18:50:27 +0100

    Fix lint errors

--- core/logging/src/main/java/com/costular/atomtasks/core/logging/NoOpLogger.kt
+++ core/logging/src/main/java/com/costular/atomtasks/core/logging/NoOpLogger.kt
@@ -1,9 +1,11 @@
 package com.costular.atomtasks.core.logging
 
 object NoOpLogger : AtomLogger {
+    @Suppress("EmptyFunctionBlock")
     override fun log(level: LogLevel, tag: String, text: String) {
     }
 
+    @Suppress("EmptyFunctionBlock")
     override fun log(
         level: LogLevel,
         tag: String,
