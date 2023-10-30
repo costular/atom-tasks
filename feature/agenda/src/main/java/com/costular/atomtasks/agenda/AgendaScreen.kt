b6d6bfa186bbfe8612e1ea83fa33ace550af8675
Diego Francisco Concepción <diego.concepcion@jobandtalent.com>
2023-10-30 11:49:55 +0000

    Fix lint error

--- feature/agenda/src/main/java/com/costular/atomtasks/agenda/AgendaScreen.kt
+++ feature/agenda/src/main/java/com/costular/atomtasks/agenda/AgendaScreen.kt
@@ -18,7 +18,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material.icons.outlined.Today
-import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Surface
