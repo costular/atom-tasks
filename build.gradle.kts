088c95c5353b8baa07f5d2ef0c7e63a55eb3062b
Diego Francisco Concepción <costular@gmail.com>
2023-11-24 17:20:59 +0000

    Enhance performance with baseline profiles (#122)

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,5 +10,6 @@ buildscript {
         classpath("com.google.dagger:hilt-android-gradle-plugin:${libs.versions.hilt.get()}")
         classpath("com.google.gms:google-services:4.3.15")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.7")
+        classpath(libs.androidx.benchmark.baseline.profile.gradle.plugin)
     }
 }
