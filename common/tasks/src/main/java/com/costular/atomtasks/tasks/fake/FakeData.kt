d88e0ad324639207055cb2da665a20e5e18a37aa
Diego Francisco Concepción <diego.concepcion@jobandtalent.com>
2023-12-17 21:22:13 +0000

    Add fake tasks

--- common/tasks/src/main/java/com/costular/atomtasks/tasks/fake/FakeData.kt
+++ common/tasks/src/main/java/com/costular/atomtasks/tasks/fake/FakeData.kt
@@ -1,8 +1,10 @@
 package com.costular.atomtasks.tasks.fake
 
 import com.costular.atomtasks.tasks.model.RecurrenceType
+import com.costular.atomtasks.tasks.model.Reminder
 import com.costular.atomtasks.tasks.model.Task
 import java.time.LocalDate
+import java.time.LocalTime
 
 val TaskToday = Task(
     id = 10L,
@@ -17,7 +19,7 @@ val TaskToday = Task(
     recurrenceType = null,
     parentId = null,
 )
-val TaskWeekly = Task(
+val TaskRecurring = Task(
     id = 11L,
     name = "Task test",
     createdAt = LocalDate.now(),
@@ -26,6 +28,53 @@ val TaskWeekly = Task(
     isDone = false,
     position = 0,
     isRecurring = true,
+    recurrenceEndDate = null,
+    recurrenceType = RecurrenceType.WEEKLY,
+    parentId = 1L,
+)
+val TaskWithReminder = Task(
+    id = 11L,
+    name = "Task test",
+    createdAt = LocalDate.now(),
+    day = LocalDate.now(),
+    reminder = Reminder(
+        id = 1L,
+        time = LocalTime.of(9, 0),
+        date = LocalDate.now(),
+    ),
+    isDone = false,
+    position = 0,
+    isRecurring = false,
+    recurrenceEndDate = null,
+    recurrenceType = RecurrenceType.WEEKLY,
+    parentId = 1L,
+)
+val TaskRecurringWithReminder = Task(
+    id = 11L,
+    name = "Task test",
+    createdAt = LocalDate.now(),
+    day = LocalDate.now(),
+    reminder = Reminder(
+        id = 1L,
+        time = LocalTime.of(9, 0),
+        date = LocalDate.now(),
+    ),
+    isDone = false,
+    position = 0,
+    isRecurring = true,
+    recurrenceEndDate = null,
+    recurrenceType = RecurrenceType.WEEKLY,
+    parentId = 1L,
+)
+val TaskFinished = Task(
+    id = 11L,
+    name = "Task test",
+    createdAt = LocalDate.now(),
+    day = LocalDate.now(),
+    reminder = null,
+    isDone = true,
+    position = 0,
+    isRecurring = false,
     recurrenceEndDate = null,
     recurrenceType = RecurrenceType.WEEKLY,
     parentId = 1L,
