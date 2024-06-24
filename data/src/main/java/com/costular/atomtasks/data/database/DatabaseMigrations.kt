7cf3944b18dd24cc868d51d400ec0e80cbec4de3
Diego Francisco Concepción <costular@gmail.com>
2024-06-24 19:02:01 +0100

    Fix wrong migration when removing is_enabled reminder column
    SQLite doesn't allow to drop a column by ALTER TABLE command

--- data/src/main/java/com/costular/atomtasks/data/database/DatabaseMigrations.kt
+++ data/src/main/java/com/costular/atomtasks/data/database/DatabaseMigrations.kt
@@ -8,34 +8,47 @@ val MIGRATION_4_5 = object : Migration(4, 5) {
     override fun migrate(db: SupportSQLiteDatabase) {
         db.execSQL(
             "CREATE TABLE IF NOT EXISTS `_new_tasks` (`id` INTEGER PRIMARY KEY" +
-                " AUTOINCREMENT NOT NULL, `created_at` TEXT NOT NULL, `name` TEXT NOT NULL," +
-                " `date` TEXT NOT NULL, `is_done` INTEGER NOT NULL, `position`" +
-                " INTEGER NOT NULL DEFAULT 0)",
+                    " AUTOINCREMENT NOT NULL, `created_at` TEXT NOT NULL, `name` TEXT NOT NULL," +
+                    " `date` TEXT NOT NULL, `is_done` INTEGER NOT NULL, `position`" +
+                    " INTEGER NOT NULL DEFAULT 0)",
         )
         db.execSQL(
             "INSERT INTO `_new_tasks` (`id`,`created_at`,`name`,`date`," +
-                "`is_done`) SELECT `id`,`created_at`,`name`,`date`,`is_done` FROM `tasks`",
+                    "`is_done`) SELECT `id`,`created_at`,`name`,`date`,`is_done` FROM `tasks`",
         )
         db.execSQL("DROP TABLE `tasks`")
         db.execSQL("ALTER TABLE `_new_tasks` RENAME TO `tasks`")
         db.execSQL(
             "UPDATE tasks SET position = (SELECT COUNT ( *) FROM tasks " +
-                "AS t WHERE t.ID <= tasks.ID);",
+                    "AS t WHERE t.ID <= tasks.ID);",
         )
         db.execSQL(
             "CREATE INDEX IF NOT EXISTS `index_tasks_created_at` " +
-                "ON `tasks` (`created_at`)",
+                    "ON `tasks` (`created_at`)",
         )
         db.execSQL(
             "CREATE UNIQUE INDEX IF NOT EXISTS `index_tasks_position_date` " +
-                "ON `tasks` (`position`, `date`)",
+                    "ON `tasks` (`position`, `date`)",
         )
     }
 }
 
 val MIGRATION_5_6 = object : Migration(5, 6) {
     override fun migrate(db: SupportSQLiteDatabase) {
-        db.execSQL("ALTER TABLE reminders DROP COLUMN is_enabled;")
+        db.execSQL(
+            """
+            CREATE TABLE IF NOT EXISTS `_new_reminders` 
+            (`reminder_id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `time` TEXT NOT NULL,
+             `date` TEXT NOT NULL, 
+             `task_id` INTEGER NOT NULL)
+             """
+        )
+        db.execSQL(
+            """INSERT INTO `_new_reminders` (`reminder_id`,`time`,`date`,`task_id`) 
+            SELECT `reminder_id`,`time`,`date`,`task_id` FROM `reminders`"""
+        )
+        db.execSQL("DROP TABLE reminders")
+        db.execSQL("ALTER TABLE `_new_reminders` RENAME TO `reminders`")
     }
 }
 
