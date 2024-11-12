00937b0d9d26508e687bda5ea01177de6f9d94b9
Diego Francisco Concepción <costular@gmail.com>
2024-11-12 22:58:24 +0000

    Remove unused code

--- core/designsystem/src/main/java/com/costular/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/com/costular/designsystem/theme/Theme.kt
@@ -91,18 +91,6 @@ private val darkScheme = darkColorScheme(
     surfaceContainerHighest = surfaceContainerHighestDark,
 )
 
-@Immutable
-data class ColorFamily(
-    val color: Color,
-    val onColor: Color,
-    val colorContainer: Color,
-    val onColorContainer: Color
-)
-
-val unspecified_scheme = ColorFamily(
-    Color.Unspecified, Color.Unspecified, Color.Unspecified, Color.Unspecified
-)
-
 @Composable
 fun ProvideDimens(
     dimensions: Dimensions,
