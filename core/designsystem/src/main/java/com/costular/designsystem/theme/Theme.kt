03392ed64c95140d39e54c85db29a4352ca3ebf8
Diego Francisco Concepción <costular@gmail.com>
2024-11-12 17:54:32 +0000

    Fix daily reminder being fired always (#145)
    * Only show notification when setting is enabled
    
    Also, to prevent executing unnecessary code and reduce the resources usage now the alarm gets canceled when the setting is disabled
    
    * Fix lint errors
    
    (cherry picked from commit 0e66aef7d772253255e29d187f9e62961322a29f)

--- core/designsystem/src/main/java/com/costular/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/com/costular/designsystem/theme/Theme.kt
@@ -9,10 +9,8 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.staticCompositionLocalOf
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
